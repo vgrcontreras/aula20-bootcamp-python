1eb105c16a25df41a11804bb0e983521dc3a4352
Victor Contreras <contreras3991@gmail.com>
2024-11-20 12:31:05 -0300

    :sparkles: feat(routers): create products router

---
+++ backend/routers/products.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from backend.database import get_session
+from backend.models import Product
+from backend.schemas import ProductCreate, ProductResponse
+
+router = APIRouter(prefix='/products', tags=['products'])
+
+@router.post('/', response_model=ProductResponse)
+def create_product(
+    product: ProductCreate,
+    db: Session = Depends(get_session),
+):
+    db_product = Product(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+
+    return db_product
