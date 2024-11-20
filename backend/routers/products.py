aaa12519355c6862728cc0363dca8aa75a435a63
Victor Contreras <contreras3991@gmail.com>
2024-11-20 13:00:23 -0300

    :sparkles: feat(products): add router to retrieve all products

--- backend/routers/products.py
+++ backend/routers/products.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from backend.database import get_session
@@ -6,6 +9,7 @@ from backend.models import Product
 from backend.schemas import ProductCreate, ProductResponse
 
 router = APIRouter(prefix='/products', tags=['products'])
+
 
 @router.post('/', response_model=ProductResponse)
 def create_product(
@@ -18,3 +22,12 @@ def create_product(
     db.refresh(db_product)
 
     return db_product
+
+@router.get('/', response_model=List[ProductResponse])
+def read_all_products(session: Session = Depends(get_session)):
+    products_db = session.scalars(
+        select(Product)
+    ).all()
+    
+    return products_db
+
