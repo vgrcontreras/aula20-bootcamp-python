421b1bc1cf5c3c81a2271460c7e46b648dda44c3
Victor Contreras <contreras3991@gmail.com>
2024-11-20 19:35:56 -0300

    :sparkles: feat(products): add delete product and patch product routers

--- backend/routers/products.py
+++ backend/routers/products.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import Session
 
 from backend.database import get_session
 from backend.models import Product
-from backend.schemas import ProductCreate, ProductResponse
+from backend.schemas import (Message, ProductCreate, ProductResponse,
+                             ProductUpdate)
 
 router = APIRouter(prefix='/products', tags=['products'])
 
@@ -50,4 +51,50 @@ def read_product(
             detail='Produto não encontrado'
         )
     
+    return product_db
+
+
+@router.delete('/{product_id}', response_model=Message)
+def delete_product(
+    product_id: int,
+    db: Session = Depends(get_session)
+):
+    product_db = db.scalar(
+    select(Product).where(Product.id == product_id)
+    )
+
+    if not product_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Produto não encontrado'
+        )
+    
+    db.delete(product_db)
+    db.commit()
+
+    return {'message': 'Product deleted successfully'}
+
+
+@router.patch('/{product_id}', response_model=ProductResponse)
+def patch_todo(
+    product_id: int,
+    product: ProductUpdate,
+    session: Session = Depends(get_session),
+):
+    product_db = session.scalar(
+        select(Product).where(Product.id == product_id)
+    )
+
+    if not product_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Product not found'
+        )
+
+    for key, value in product.model_dump(exclude_unset=True).items():
+        setattr(product_db, key, value)
+
+    session.add(product_db)
+    session.commit()
+    session.refresh(product_db)
+
     return product_db
